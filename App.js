355a5d1ebb25cea964e04b79d62680dfe21a8002
Danilovir <danilo.vieira7@fatec.sp.gov.br>
2024-03-04 17:35:23 -0300

    form label input

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Title from './src/components/title/';
+import Main from './src/components/Main/';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <Title/>
+      <Main/>
+      </View>
   );
 }
 
---
+++ src/components/form/index.js
@@ -0,0 +1,59 @@
+
+import React, { useState } from "react";
+import { TextInput, View, Text, Button } from "react-native"
+import ResultImc from "./ResultImc/Index";
+
+export default function Form(){
+
+    const [height, setHeitght] = useState(null)
+    const [weight, setWeght] = useState(null)
+    const [messageImc, setMassageImc] = useState("preencha o peso e altura");
+    const [imc, setImc] = useState(null)
+    const [textButton, setTextButton] = useState("Calcular IMC")
+
+
+    function imcCalculator(){
+        return((weight/(height*height)).toFixed(2))
+    }
+
+        function validationImc(){
+            if(weight != null && height != null){
+                imcCalculator()
+                setHeitght(null)
+                setWeght(null)
+                setMassageImc("Seu imc é igual: ")
+                setTextButton("Calcular Novamente")
+                return
+            }
+
+            setImc(null)
+            setTextButton("Calcular")
+            setMassageImc("Preencha peso e Altura")
+        }
+
+    return(
+        <View>
+            <View>
+                <Text>Altura</Text>
+                <TextInput
+                onChange={setHeitght}
+                value={height}
+              //  placeholder="EX. 1.75"
+              //  keyboardType="numeric"
+                ></TextInput>
+
+                <Text>Peso</Text>
+                <TextInput
+                onChange={setWeght}
+                value={weight}
+                 // placeholder="EX. 75.365"
+                 // keyboardType="numeric"
+                ></TextInput>
+                <Button
+                onPress={() => validationImc()}
+                 title={textButton}/>
+            </View>
+            <ResultImc messageResultImc={messageImc} resultImc={imc}/>
+        </View>
+    );
+}
