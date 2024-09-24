098601e4f8086d2ec45e5ec3d5b38df5104ce342
mariajhuliamira <maria.mira@etec.sp.gov.br>
2024-09-24 11:58:00 -0300

    Aula 24 - 09

--- src/components/form/ResultImc/style.js
+++ src/components/form/ResultImc/style.js
@@ -25,6 +25,27 @@ const styles = StyleSheet.create({
         fontWeight:"bold",
 
     },
+    titleResultImc:{
+        fontSize:18,
+        color:"ff0043",
+        fontWeight:"bold",
+    },
+    boxShareButton:{
+        width: "100%",
+        alignItems:"center",
+        marginBottom:10,
+    },
+    shared:{
+        backgroundColor:"#1877f2",
+        borderRadius:50,
+        paddingBottom:5,
+        paddingTop:5,
+    },
+    sharedText:{
+        color:"#ffffff",
+        fontWeight:"bold",
+        paddingHorizonta:30,
+    }
   
 });
 
